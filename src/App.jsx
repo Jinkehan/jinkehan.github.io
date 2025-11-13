487d20a76035193f4fed99bcb266b9da76f936e2
Jinkehan <98069250+Jinkehan@users.noreply.github.com>
2025-11-13 13:06:20 -0800

    Basic setup complete (#1)
    Set up a top tab bar, with an empty home page and about page to be completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-function App() {
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import './App.css'
+
+// Map routes to their background classes
+const pageBackgrounds = {
+  '/': 'home-background',
+  '/about': 'about-background',
+}
+
+function AppContent() {
+  const location = useLocation()
+  const backgroundClass = pageBackgrounds[location.pathname] || 'default-background'
+
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="app">
+      <Header />
+      <main className={`main-content ${backgroundClass}`}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
     </div>
   )
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  )
+}
 
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="page">
+      <h1>home</h1>
+    </div>
+  )
+}
+
+export default Home
+
