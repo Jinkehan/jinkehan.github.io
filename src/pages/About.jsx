ad6041c1b397d7ce510102b73f5c67ac3d308811
Kehan Jin <98069250+Jinkehan@users.noreply.github.com>
2025-11-13 13:05:52 -0800

    Basic setup complete
    Set up a top tab bar, with an empty home page and about page to be completed

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div className="page">
+      <h1>About</h1>
+      <p>
+        Hi, I'm Jin Kehan! I'm a passionate developer and student interested in 
+        building innovative web applications and exploring the latest technologies. 
+        I enjoy working on projects that combine creativity with technical problem-solving, 
+        and I'm always eager to learn new skills and take on exciting challenges.
+      </p>
+    </div>
+  )
+}
+
+export default About
+
